54b37d122f767c7ed294bc6fc0500718cd638033
Stephen David Tumala Mendoza <stephenmendoza@Stephens-MacBook-Pro.local>
2026-06-03 22:45:28 -0400

    reduce top padding content

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ div[data-testid="stDataFrame"] > div { background: white !important; }
 }
 
 /* CONTENT */
-.content { padding: 2rem 7rem 3.5rem 7rem; }
+.content { padding: 0.5rem 7rem 3.5rem 7rem; }
 
 /* SECTION LABEL */
 .sec-label {
